c4db5d802ece2b4fea2a4bd073d1cf5d0b0fe69d
Sebastian <72668007+SebaSanchezI@users.noreply.github.com>
2025-02-09 15:39:56 +0100

    Add new name

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard-with-typescript", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+        project: "./tsconfig.json",
+      },
     },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react/prop-types": [2, { ignore: ["children"] }],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script",
-                "project": "./tsconfig.json"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "react/prop-types": [2, { ignore: ['children'] }],
-        "@typescript-eslint/no-misused-promises": [2, {
-            "checksVoidReturn": {
-              "attributes": false
-            }
-          }]
-    }
-}
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { CircularProgress, CssBaseline, ThemeProvider } from '@mui/material'
-import { useThemeContext } from '@context/theme/themeContextProvider'
-import { Navbar } from '@components/Navbar'
-import Landing from './pages/landing/Landing'
-import { Suspense } from 'react'
+import { CircularProgress, CssBaseline, ThemeProvider } from "@mui/material";
+import { useThemeContext } from "@context/theme/themeContextProvider";
+import { Navbar } from "@components/Navbar";
+import Landing from "./pages/landing/Landing";
+import { Suspense } from "react";
+import Hero from "@components/hero/hero";
 
-export function App () {
-  const { theme } = useThemeContext()
+export function App() {
+  const { theme } = useThemeContext();
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Navbar />
+      <Hero />
       <Landing />
     </ThemeProvider>
-  )
+  );
 }
 
-export default function WrapperApp () {
+export default function WrapperApp() {
   <Suspense fallback={<CircularProgress />}>
     <App />
-  </Suspense>
+  </Suspense>;
 }
