579759cfb8c8b9d6cd0ebe4f6be67ccd84fcb103
Sebastian <72668007+SebaSanchezI@users.noreply.github.com>
2025-02-09 15:38:54 +0100

    Change article

--- src/components/article/SectionComponent.tsx
+++ src/components/article/SectionComponent.tsx
@@ -1,31 +1,42 @@
-import React, { type ReactNode } from 'react'
-import { Box, Typography } from '@mui/material'
+import React, { type ReactNode } from "react";
+import { Box, Typography } from "@mui/material";
 
 interface SectionComponentProps {
-  sectionId: string
-  title: string
-  fullscreen?: boolean
-  children: ReactNode
-  ref: any
+  sectionId: string;
+  title?: string;
+  fullscreen?: boolean;
+  children: ReactNode;
+  ref: any;
+  style?: any;
 }
 
-const SectionComponent: React.FC<SectionComponentProps> = ({ sectionId, title, fullscreen, children, ref }) => {
+const SectionComponent: React.FC<SectionComponentProps> = ({
+  sectionId,
+  title,
+  fullscreen,
+  children,
+  ref,
+  style,
+}) => {
   return (
     <Box
       id={sectionId}
-      component='section'
-      display='flex'
-      flexDirection='column'
-      marginBottom='64px'
-      minHeight={(fullscreen !== null && fullscreen !== undefined) ? '100vh' : 0}
+      component="section"
+      display="flex"
+      flexDirection="column"
+      marginBottom="64px"
+      minHeight={fullscreen !== null && fullscreen !== undefined ? "100vh" : 0}
       ref={ref}
+      style={style}
     >
-      <Typography variant='h3' textAlign='start' marginBottom='16px'>
-        {title}
-      </Typography>
+      {!!(title != null && title !== "") && (
+        <Typography variant="h3" textAlign="start" marginBottom="16px">
+          {title}
+        </Typography>
+      )}
       {children}
     </Box>
-  )
-}
+  );
+};
 
-export default SectionComponent
+export default SectionComponent;
