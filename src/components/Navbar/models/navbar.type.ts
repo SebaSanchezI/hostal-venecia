215656199ff289ada79df75f5beaf426b590dacb
Sebastian <72668007+SebaSanchezI@users.noreply.github.com>
2025-02-09 15:38:25 +0100

    Change data

--- src/components/Navbar/models/navbar.type.ts
+++ src/components/Navbar/models/navbar.type.ts
@@ -1,16 +1,16 @@
 export interface NavItem {
-  id: number
-  label: string
-  path: string
-  iconName: string
+  id: number;
+  label: string;
+  path: string;
+  iconName: string;
 }
 
 export enum SocialMediaName {
-  INSTAGRAM = 'instagram',
-  FACEBOOK = 'facebook'
+  INSTAGRAM = "instagram",
+  FACEBOOK = "facebook",
 }
 
 export enum SocialMediarUrl {
-  INSTAGRAM = 'https://www.instagram.com/',
-  FACEBOOK = 'https://www.facebook.com/'
+  INSTAGRAM = "https://www.instagram.com/hostal_venecia",
+  FACEBOOK = "https://www.facebook.com/groups/351844744964700/user/61571699947053",
 }
--- src/components/contact/data/contactData.ts
+++ src/components/contact/data/contactData.ts
@@ -1,16 +1,12 @@
 export const CONTACT_DATA = {
-  ADDRESS: 'Av. del Ejército 27, 22609 Sabiñánigo, Huesca, España',
-  PHONE: '34657479796',
-  EMAIL: 'alberguejuvenilpirenarium@gmail.com'
-}
+  ADDRESS: "C. de Lope de Vega, 22400 Monzon, Huesca, España",
+  PHONE: "34974403699",
+  EMAIL: "hostal-venecia@hotmail.com",
+};
 
 export const BOOK_LINKS = {
   BOOKING: {
-    name: 'Booking.com',
-    url: 'https://www.booking.com/hotel/es/albergue-pirenarium-sabinanigo.es.html?aid=304142&label=Share-PljAdo%401693486549'
+    name: "Booking.com",
+    url: "https://www.booking.com/hotel/es/hostal-venecia-monzon.es.html?aid=303946&label=monzon-rXvzLwFUroEnYJehTHw8nQS500059952157%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-952878396659%3Alp1005471%3Ali%3Adec%3Adm&sid=4a82760ebb15f03e35cd32fb29c9d861&dest_id=-392896&dest_type=city&dist=0&group_adults=2&group_children=0&hapos=3&hpos=3&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA&sb_price_type=total&sr_order=popularity&srepoch=1737822202&srpvid=eaf073378b8202ff&type=total&ucfs=1&",
   },
-  DIRECT_BOOK: {
-    name: 'DirectBook.com',
-    url: 'https://direct-book.com/properties/alberguepirenarium-direct?locale=en&items[0][adults]=2&items[0][children]=0&items[0][infants]=0&currency=EUR&checkInDate=2023-10-03&checkOutDate=2023-10-04&trackPage=yes'
-  }
-}
+};
