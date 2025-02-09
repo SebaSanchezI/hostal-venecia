3ba53b4d3a3ec74f74c32d292a76519873ed1b83
Sebastian <72668007+SebaSanchezI@users.noreply.github.com>
2025-02-09 15:39:13 +0100

    Change components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { useThemeContext } from '@context/theme/themeContextProvider'
-import MenuIcon from '@mui/icons-material/Menu'
-import InstagramIcon from '@mui/icons-material/Instagram'
-import FacebookIcon from '@mui/icons-material/Facebook'
+import { useThemeContext } from "@context/theme/themeContextProvider";
+import MenuIcon from "@mui/icons-material/Menu";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
 import {
   AppBar,
   Drawer,
@@ -12,112 +12,131 @@ import {
   ListItem,
   ListItemButton,
   ListItemText,
-  Box
-} from '@mui/material'
-import { SocialMediaName, type NavItem, SocialMediarUrl } from './models/navbar.type'
-import { NavListDrawer } from './components/NavListDrawer'
-import { LIST_ITEMS_NAV } from './data'
-import ThemeModeToggle from '@shared/ThemeModeToggle/ThemeModeToggle'
-import logoPath from '@assets/logo-chrome.png'
-import SelectLanguage from '@components/selectLanguage/SelectLanguage'
-import { LANGUAGE_LIST } from '@components/selectLanguage/data/languageList'
-import { useTranslation } from 'react-i18next'
+  Box,
+} from "@mui/material";
+import {
+  SocialMediaName,
+  type NavItem,
+  SocialMediarUrl,
+} from "./models/navbar.type";
+import { NavListDrawer } from "./components/NavListDrawer";
+import { LIST_ITEMS_NAV } from "./data";
+import ThemeModeToggle from "@shared/ThemeModeToggle/ThemeModeToggle";
+import logoPath from "@assets/logo-venecia.png";
+import SelectLanguage from "@components/selectLanguage/SelectLanguage";
+import { LANGUAGE_LIST } from "@components/selectLanguage/data/languageList";
+import { useTranslation } from "react-i18next";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
-  const { theme } = useThemeContext()
-  const { t } = useTranslation()
+  const { theme } = useThemeContext();
+  const { t } = useTranslation();
 
-  const [open, setOpen] = useState<boolean>(false)
-  const [hash, setHash] = useState<string>(LIST_ITEMS_NAV[0].path)
+  const [open, setOpen] = useState<boolean>(false);
+  const [hash, setHash] = useState<string>(LIST_ITEMS_NAV[0].path);
 
   const handleSendTo = (name: SocialMediaName) => {
-    const url = name === SocialMediaName.INSTAGRAM ? SocialMediarUrl.INSTAGRAM : SocialMediarUrl.FACEBOOK
-    const win = window.open(url, '_blank')
-    win?.focus()
-  }
+    const url =
+      name === SocialMediaName.INSTAGRAM
+        ? SocialMediarUrl.INSTAGRAM
+        : SocialMediarUrl.FACEBOOK;
+    const win = window.open(url, "_blank");
+    win?.focus();
+  };
 
   const handleClicButton = (path: string): void => {
-    setHash(path)
-    const sectionId: string = path.slice(1)
-    const section = document.getElementById(sectionId)
+    setHash(path);
+    const sectionId: string = path.slice(1);
+    const section = document.getElementById(sectionId);
     if (section != null) {
       section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'end'
-      })
+        behavior: "smooth",
+        block: "end",
+      });
     }
-  }
+  };
 
   const handleClicListDrawer = (path: string): void => {
-    handleClicButton(path)
-    setOpen(false)
-  }
+    handleClicButton(path);
+    setOpen(false);
+  };
 
   return (
     <>
-      <AppBar sx={{ padding: { xs: '8px 16px', sm: '8px 16px', md: '0 16px' } }}>
-        <Grid container alignItems='center' justifyContent='space-between'>
-          <Grid item padding='0 16px'>
+      <AppBar
+        sx={{ padding: { xs: "8px 16px", sm: "8px 16px", md: "0 16px" } }}
+      >
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item padding="0 16px">
             <IconButton
-              size='large'
-              onClick={() => { setOpen(true) }}
-              sx={{ display: { xs: 'bloke', sm: 'bloke', md: 'none' } }}
+              size="large"
+              onClick={() => {
+                setOpen(true);
+              }}
+              sx={{ display: { xs: "bloke", sm: "bloke", md: "none" } }}
             >
-              <MenuIcon color='primary' sx={{ fontSize: '30px' }} />
+              <MenuIcon color="primary" sx={{ fontSize: "30px" }} />
             </IconButton>
             <Box
-              component='img'
+              component="img"
               sx={{
-                height: 80,
-                width: 100,
-                maxHeight: { md: 80 },
-                maxWidth: { md: 100 },
-                display: { xs: 'none', sm: 'none', md: 'block' },
-                padding: '8px'
+                height: 60,
+                width: 60,
+                maxHeight: { md: 200 },
+                maxWidth: { md: 200 },
+                display: { xs: "none", sm: "none", md: "block" },
               }}
-              alt='Pirenarium Logo'
+              alt="Venecia Logo"
               src={logoPath}
             />
           </Grid>
-          <Grid item sx={{ display: { xs: 'none', sm: 'none', md: 'block' } }}>
+          <Grid item sx={{ display: { xs: "none", sm: "none", md: "block" } }}>
             <ListItem>
               {LIST_ITEMS_NAV.map(({ id, label, path }: NavItem) => (
                 <ListItemButton
                   key={id}
                   selected={hash === path}
-                  onClick={() => { handleClicButton(path) }}
-                  >
-                  <ListItemText primary={t(`titles.${label}`)} primaryTypographyProps={{ fontWeight: 600 }} />
+                  onClick={() => {
+                    handleClicButton(path);
+                  }}
+                >
+                  <ListItemText
+                    primary={t(`titles.${label}`)}
+                    primaryTypographyProps={{ fontWeight: 600 }}
+                  />
                 </ListItemButton>
               ))}
             </ListItem>
           </Grid>
           <Grid
             item
-            padding='0 16px'
+            padding="0 16px"
             sx={{
-              display: { xs: 'none', sm: 'none', md: 'flex' },
-              flexDirection: 'row',
-              alignItems: 'center'
+              display: { xs: "none", sm: "none", md: "flex" },
+              flexDirection: "row",
+              alignItems: "center",
             }}
           >
             <SelectLanguage languages={LANGUAGE_LIST} />
             <IconButton
-              size='large'
-              onClick={() => { handleSendTo(SocialMediaName.INSTAGRAM) }}
-              sx={{ marginLeft: '8px' }}
+              size="large"
+              onClick={() => {
+                handleSendTo(SocialMediaName.INSTAGRAM);
+              }}
+              sx={{ marginLeft: "8px" }}
             >
-              <InstagramIcon color='primary' />
+              <InstagramIcon color="primary" />
             </IconButton>
             <IconButton
-              size='large'
-              onClick={() => { handleSendTo(SocialMediaName.FACEBOOK) }}
-             sx={{ marginLeft: '8px' }}
+              size="large"
+              onClick={() => {
+                handleSendTo(SocialMediaName.FACEBOOK);
+              }}
+              sx={{ marginLeft: "8px" }}
             >
-              <FacebookIcon color='primary' />
+              <FacebookIcon color="primary" />
             </IconButton>
             <ThemeModeToggle />
           </Grid>
@@ -125,9 +144,11 @@ const Navbar: React.FC<NavbarProps> = () => {
       </AppBar>
       <Drawer
         open={open}
-        anchor='left'
-        onClose={() => { setOpen(false) }}
-        sx={{ display: { xs: 'bloke', sm: 'bloke', md: 'none' } }}
+        anchor="left"
+        onClose={() => {
+          setOpen(false);
+        }}
+        sx={{ display: { xs: "bloke", sm: "bloke", md: "none" } }}
       >
         <NavListDrawer
           listItems={LIST_ITEMS_NAV}
@@ -138,7 +159,7 @@ const Navbar: React.FC<NavbarProps> = () => {
         />
       </Drawer>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,52 +1,61 @@
-import { SECTION_IDS } from '@global/constants'
+import { SECTION_IDS } from "@global/constants";
 
-import { ImageSlider, SectionComponent } from '@components/index'
-import ServicesListCards from '@components/servicesListCards'
+import { SectionComponent } from "@components/index";
+import ServicesListCards from "@components/servicesListCards";
 
-import ImageContainer from '@components/imageList/ImageContainer'
-import AccordionList from '@components/accordionList/AcordionList'
-import Contact from '@components/contact/Contact'
+import ImageContainer from "@components/imageList/ImageContainer";
+import AccordionList from "@components/accordionList/AcordionList";
+import Contact from "@components/contact/Contact";
 
-import { Slices } from '@components/imageSlider/data'
-import { IMAGE_ROOM_LIST } from '@components/imageList/data/imageListData'
-import { LIST_CARDS } from '@components/servicesListCards/data/data'
-import { ACCORDION_DATA_LIST } from '@components/accordionList/data/accordionDataList'
-import { useTranslation } from 'react-i18next'
-import { Container } from '@mui/material'
-import { useRef } from 'react'
+import { IMAGE_ROOM_LIST } from "@components/imageList/data/imageListData";
+import { LIST_CARDS } from "@components/servicesListCards/data/data";
+import { ACCORDION_DATA_LIST } from "@components/accordionList/data/accordionDataList";
+import { useTranslation } from "react-i18next";
+import { Container } from "@mui/material";
+import { useRef } from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface LandingProps {}
 
 const Landing: React.FC<LandingProps> = () => {
-  const homeRef = useRef()
-  const serviceRef = useRef()
-  const roomsRef = useRef()
-  const faqRef = useRef()
-  const contactRef = useRef()
-  const { t } = useTranslation()
+  const serviceRef = useRef();
+  const roomsRef = useRef();
+  const faqRef = useRef();
+  const contactRef = useRef();
+  const { t } = useTranslation();
 
   return (
-    <Container
-    sx={{ paddingTop: '98px', paddingBottom: '24px' }}
-    >
-      <SectionComponent title={t('titles.home')} sectionId={SECTION_IDS.HOME} ref={homeRef}>
-        <ImageSlider slices={Slices} autoPlay width='100%' height='480px' />
-      </SectionComponent>
-      <SectionComponent title={t('titles.services')} sectionId={SECTION_IDS.SERVICES} ref={serviceRef}>
+    <Container sx={{ paddingTop: "98px", paddingBottom: "24px" }}>
+      <SectionComponent
+        title={t("titles.services")}
+        sectionId={SECTION_IDS.SERVICES}
+        ref={serviceRef}
+      >
         <ServicesListCards list={LIST_CARDS} />
       </SectionComponent>
-      <SectionComponent title={`${t('titles.rooms')} y ${t('titles.commonSpaces')}`} sectionId={SECTION_IDS.ROOMS} ref={roomsRef}>
-        <ImageContainer list={IMAGE_ROOM_LIST } />
+      <SectionComponent
+        title={`${t("titles.rooms")} y ${t("titles.commonSpaces")}`}
+        sectionId={SECTION_IDS.ROOMS}
+        ref={roomsRef}
+      >
+        <ImageContainer list={IMAGE_ROOM_LIST} />
       </SectionComponent>
-      <SectionComponent title={t('titles.faq')} sectionId={SECTION_IDS.FAQ} ref={faqRef}>
-        <AccordionList list={ACCORDION_DATA_LIST}/>
+      <SectionComponent
+        title={t("titles.faq")}
+        sectionId={SECTION_IDS.FAQ}
+        ref={faqRef}
+      >
+        <AccordionList list={ACCORDION_DATA_LIST} />
       </SectionComponent>
-      <SectionComponent title={t('titles.contact')} sectionId={SECTION_IDS.CONTACT} ref={contactRef}>
+      <SectionComponent
+        title={t("titles.contact")}
+        sectionId={SECTION_IDS.CONTACT}
+        ref={contactRef}
+      >
         <Contact />
       </SectionComponent>
     </Container>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
