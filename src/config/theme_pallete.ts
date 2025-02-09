a68b4b60e018894c195bb367997911f12c80be26
Sebastian <72668007+SebaSanchezI@users.noreply.github.com>
2025-02-09 15:37:24 +0100

    Change theme

--- src/config/theme_pallete.ts
+++ src/config/theme_pallete.ts
@@ -1,41 +1,42 @@
 export const THEME_PALLETE_LIGHT = {
   primary: {
-    main: '#14599c',
-    hover: '#0081ff24',
-    selected: '#14599c'
+    main: "#b25e34",
+    hover: "#c4dde475",
+    selected: "#14599c",
   },
   secondary: {
-    main: '#fdc071'
+    main: "#b25e34",
   },
   background: {
-    default: '#f2f4f6',
-    paper: '#f2f4f6',
-    navbar: '#f2f4f6cc',
-    white: '#FFFFFF'
+    default: "#ebdeb8",
+    paper: "#ecdaa6",
+    navbar: "#f2f4f600",
+    white: "#FFFFFF",
   },
   text: {
-    primary: '#595d62',
-    secondary: '#14599c',
-    disabled: '#e8e8e8'
-  }
-}
+    primary: "#595d62",
+    secondary: "#b25e34",
+    disabled: "#e8e8e8",
+  },
+};
 
 export const THEME_PALLETE_DARK = {
   primary: {
-    main: '#14599c'
+    main: "#14599c",
+    hover: "#0081ff24",
   },
   secondary: {
-    main: '#fdc071'
+    main: "#fdc071",
   },
   background: {
-    default: '#0e1924',
-    paper: '#172a3d',
-    navbar: '#0e1924cc',
-    white: '#FFFFFF'
+    default: "#0e1924",
+    paper: "#172a3d",
+    navbar: "#0e1924cc",
+    white: "#FFFFFF",
   },
   text: {
-    primary: '#ffffff',
-    secondary: '#b0b8c4',
-    disabled: '#e8e8e8'
-  }
-}
+    primary: "#ffffff",
+    secondary: "#b0b8c4",
+    disabled: "#e8e8e8",
+  },
+};
--- src/context/theme/theme.ts
+++ src/context/theme/theme.ts
@@ -1,81 +1,95 @@
-import { THEME_PALLETE_DARK, THEME_PALLETE_LIGHT } from '@config/theme_pallete'
-import { type PaletteMode } from '@mui/material'
-import { ThemeModes } from './models/theme.type'
+import { THEME_PALLETE_DARK, THEME_PALLETE_LIGHT } from "@config/theme_pallete";
+import { type PaletteMode } from "@mui/material";
+import { ThemeModes } from "./models/theme.type";
 
 export const getDesignTokens = (mode: PaletteMode) => ({
   typography: {
     fontFamily: "'Rubik', sans-serif",
     h3: {
-      color: `${mode === ThemeModes.LIGHT
-        ? THEME_PALLETE_LIGHT.text.primary
-        : THEME_PALLETE_DARK.text.primary
-        }`,
-      fontSize: '2rem',
-      fontWeight: 600
-    }
+      color: `${
+        mode === ThemeModes.LIGHT
+          ? THEME_PALLETE_LIGHT.text.primary
+          : THEME_PALLETE_DARK.text.primary
+      }`,
+      fontSize: "2rem",
+      fontWeight: 600,
+    },
   },
   palette: {
     mode,
-    ...(
-      mode === ThemeModes.LIGHT
-        ? THEME_PALLETE_LIGHT
-        : THEME_PALLETE_DARK
-    )
+    ...(mode === ThemeModes.LIGHT ? THEME_PALLETE_LIGHT : THEME_PALLETE_DARK),
   },
   components: {
     MuiAppBar: {
       defaultProps: {
         style: {
-          backgroundColor: `${mode === ThemeModes.LIGHT
-            ? THEME_PALLETE_LIGHT.background.navbar
-            : THEME_PALLETE_DARK.background.navbar
-            }`,
-          backdropFilter: 'blur(16px)',
-          boxShadow: 'none',
-          backgroundImage: 'none'
-        }
-      }
+          backgroundColor: `${
+            mode === ThemeModes.LIGHT
+              ? THEME_PALLETE_LIGHT.background.navbar
+              : THEME_PALLETE_LIGHT.background.navbar
+          }`,
+          backdropFilter: "blur(16px)",
+          boxShadow: "none",
+          backgroundImage: "none",
+        },
+      },
     },
     MuiDrawer: {
       styleOverrides: {
         paper: {
-          backgroundColor: `${mode === ThemeModes.LIGHT
-            ? THEME_PALLETE_LIGHT.background.paper
-            : THEME_PALLETE_DARK.background.paper
-            }`,
-          backgroundImage: 'none'
-        }
-      }
+          backgroundColor: `${
+            mode === ThemeModes.LIGHT
+              ? THEME_PALLETE_LIGHT.background.default
+              : THEME_PALLETE_DARK.background.paper
+          }`,
+          backgroundImage: "none",
+        },
+      },
     },
     MuiListItemButton: {
       styleOverrides: {
         root: {
-          boxShadow: 'none',
-          color: `${mode === ThemeModes.LIGHT ? THEME_PALLETE_LIGHT.text.primary : THEME_PALLETE_DARK.text.primary}`,
-          '&:hover': {
-            backgroundColor: THEME_PALLETE_LIGHT.primary.hover
+          boxShadow: "none",
+          color: `${
+            mode === ThemeModes.LIGHT
+              ? THEME_PALLETE_LIGHT.text.primary
+              : THEME_PALLETE_DARK.text.primary
+          }`,
+          "&:hover": {
+            backgroundColor:
+              mode === ThemeModes.LIGHT
+                ? THEME_PALLETE_LIGHT.primary.hover
+                : THEME_PALLETE_DARK.primary.hover,
           },
-          '&.Mui-selected': {
-            backgroundColor: 'transparent',
-            borderBottom: `4px solid ${THEME_PALLETE_LIGHT.primary.main}`
-          }
-        }
-      }
+          "&.Mui-selected": {
+            backgroundColor: "transparent",
+            borderBottom: `4px solid ${
+              mode === ThemeModes.LIGHT
+                ? THEME_PALLETE_LIGHT.primary.main
+                : THEME_PALLETE_DARK.primary.main
+            }`,
+          },
+        },
+      },
     },
     MuiIconButton: {
       styleOverrides: {
         root: {
-          padding: '4px',
-          borderRadius: '10px',
-          border: '1px solid',
-          borderColor: `${mode === ThemeModes.LIGHT ? 'transparent' : '#1f262e'}`,
-          '&:hover': {
-            borderColor: ` ${mode === ThemeModes.LIGHT ? 'transparent' : '#2f3a46'}`
-          }
-        }
-      }
-    }
-  }
-})
+          padding: "4px",
+          borderRadius: "10px",
+          border: "1px solid",
+          borderColor: `${
+            mode === ThemeModes.LIGHT ? "transparent" : "#1f262e"
+          }`,
+          "&:hover": {
+            borderColor: ` ${
+              mode === ThemeModes.LIGHT ? "transparent" : "#2f3a46"
+            }`,
+          },
+        },
+      },
+    },
+  },
+});
 
-export default THEME_PALLETE_LIGHT
+export default THEME_PALLETE_LIGHT;
